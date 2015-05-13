23a80be28119d7ee66489e935571e6b22a6f05f6
Min RK <benjaminrk@gmail.com>
2015-05-13 10:56:32 -0700

    s/jupyter_notebook/notebook

---
+++ notebook/static/base/js/namespace.js
@@ -0,0 +1,10 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
+
+var IPython = IPython || {};
+define([], function(){
+    "use strict";
+    IPython.version = "4.0.0.dev";
+    IPython._target = '_blank';
+    return IPython;
+});
