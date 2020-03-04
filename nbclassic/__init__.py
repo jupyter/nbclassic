7525c156333542105942c090d06dd8779e820507
Zsailer <zachsailer@gmail.com>
2020-03-03 16:08:50 -0800

    load extension from NotebookApp class

--- nbclassic/__init__.py
+++ nbclassic/__init__.py
@@ -1,12 +1,4 @@
 from .notebookapp import NotebookApp
 
-# EXTENSION_NAME = "nbclassic"
-
-# def _jupyter_server_extension_paths():
-#     return [{
-#         "module": EXTENSION_NAME,
-#         "app": NotebookApp
-#     }]
 
 _jupyter_server_extension_paths = NotebookApp._jupyter_server_extension_paths
-load_jupyter_server_extension = NotebookApp.load_jupyter_server_extension
