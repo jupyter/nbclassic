aeb27b731693889baff93d948a8f8895eba59ef1
Eric Charles <eric@datalayer.io>
2022-06-14 08:49:23 +0200

    initial shim to support the notebook extensions

--- nbclassic/__init__.py
+++ nbclassic/__init__.py
@@ -1,13 +1,26 @@
 import os
+import sys
 from ._version import __version__ 
 
 # Packagers: modify this line if you store the notebook static files elsewhere
 DEFAULT_STATIC_FILES_PATH = os.path.join(os.path.dirname(__file__), "static")
 
-# Packagers: modify the next line if you store the notebook template files
-# elsewhere
+# Notebook shim to ensure notebook extensions do not break.
+try:
+    from notebook._version import __version__ as notebook_version
+    if notebook_version < "7":
+        from .shim_notebook import shim_notebook_6
+        shim_notebook_6()
+    else:
+        from .shim_notebook import shim_notebook_7_and_above
+        shim_notebook_7_and_above()
+except:
+    # Notebook is not available on the platform.
+    # We just shim the complete notebook module.
+    import jupyter_server
+    sys.modules["notebook"] = jupyter_server
 
-# Include both notebook/ and notebook/templates/.  This makes it
+# Include both nbclassic/ and nbclassic/templates/.  This makes it
 # possible for users to override a template with a file that inherits from that
 # template.
 #
