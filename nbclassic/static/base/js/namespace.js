04c24d32120eba40473141d80d4fa1e9f0627628
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2022-07-02 03:18:59 +0000

    Publish 0.4.0
    SHA256 hashes:
    
    nbclassic-0.4.0-py3-none-any.whl: 994684329411391f0893cefee98baa06ce990ab0a542c8f68679f787df6d71e0
    
    nbclassic-0.4.0.tar.gz: 712d0dcee238a435d6a7f3c9c48e85d8ef62a2b6da989e0ae5f85c51848be196

--- nbclassic/static/base/js/namespace.js
+++ nbclassic/static/base/js/namespace.js
@@ -73,7 +73,7 @@ define(function(){
     // tree
     jglobal('SessionList','tree/js/sessionlist');
 
-    Jupyter.version = "0.3.7";
+    Jupyter.version = "0.4.0";
     Jupyter._target = '_blank';
 
     return Jupyter;
