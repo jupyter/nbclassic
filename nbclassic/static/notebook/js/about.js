4f1e9273add02aabe3dc4199040664320b9b3cd7
Eric Charles <eric@datalayer.io>
2022-09-12 03:27:55 +0200

    ui: about box to display nbclassic instead of notebook (#147)

--- nbclassic/static/notebook/js/about.js
+++ nbclassic/static/notebook/js/about.js
@@ -11,15 +11,17 @@ requirejs([
     $('#notebook_about').click(function () {
         // use underscore template to auto html escape
         if (sys_info) {
-          var text = i18n.msg._('You are using Jupyter notebook.');
+          var text = i18n.msg._('You are using Jupyter NbClassic.');
           text = text + '<br/><br/>';
-          text = text + i18n.msg._('The version of the notebook server is: ');
-          text = text + _.template('<b><%- version %></b>')({ version: sys_info.notebook_version });
+          text = text + i18n.msg._('The version of the Jupyter server is: ');
+          text = text + _.template('<b><%- version %></b>')({ version: sys_info.jupyter_server_version });
           if (sys_info.commit_hash) {
               text = text + _.template('-<%- hash %>')({ hash: sys_info.commit_hash });
           }
-         text = text + '<br/>';
-         text = text + i18n.msg._('The server is running on this version of Python:');
+          text = text + '<br/>';
+          text = text + 'To get the version of nbclassic, run in a terminal: jupyter nbclassic --version';
+          text = text + '<br/>';
+          text = text + i18n.msg._('The server is running on this version of Python:');
           text = text + _.template('<br/><pre>Python <%- pyver %></pre>')({ 
             pyver: sys_info.sys_version });
           var kinfo = $('<div/>').attr('id', '#about-kinfo').text(i18n.msg._('Waiting for kernel to be available...'));
@@ -35,7 +37,7 @@ requirejs([
           body.append($('<p/>').html(text));
         }
         dialog.modal({
-            title: i18n.msg._('About Jupyter Notebook'),
+            title: i18n.msg._('About Jupyter NbClassic'),
             body: body,
             buttons: { 'OK': {} }
         });
