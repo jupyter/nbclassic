cd19fe1a9ed8fada990bdeb13ebe89085572c6cf
RRosio <rosio_reyes@yahoo.com>
2022-10-06 11:10:50 -0700

    cleaup shutdown test and refactor test_save_readonly_as, added FrontendElement type method

--- nbclassic/tests/end_to_end/test_save_readonly_as.py
+++ nbclassic/tests/end_to_end/test_save_readonly_as.py
@@ -5,9 +5,6 @@ from .utils import EDITOR_PAGE, TREE_PAGE
 import time
 
 
-# TODO: TEST TIMESOUT WHEN NOT RUN IN DEBUG MODE: DUE TO wait_for_selector('.cell') CALL IN _open_notebook_editor_page
-        # WHEN IN DEBUG MODE, HAVE TO RELOAD THE PAGE FOR THE .cell SELECTOR TO BE PICKED UP
-        # determine when to wait for the selector.....
 def check_for_rename(nb, selector, page, new_name):
     check_count = 0
     nb_name = nb.locate(selector, page)
@@ -31,7 +28,7 @@ def set_notebook_name(nb, name):
 def test_save_notebook_as(notebook_frontend):
     notebook_frontend.edit_cell(index=0, content='a=10; print(a)')
     notebook_frontend.wait_for_kernel_ready()
-    notebook_frontend.editor_page.wait_for_selector(".input")
+    notebook_frontend.wait_for_selector(".input", page=EDITOR_PAGE)
 
     # Set a name for comparison later
     set_notebook_name(notebook_frontend, name="nb1.ipynb")
@@ -39,22 +36,16 @@ def test_save_notebook_as(notebook_frontend):
 
     # Wait for Save As modal, save
     save_as(notebook_frontend)
-    notebook_frontend.editor_page.wait_for_selector('.save-message')
+    notebook_frontend.wait_for_selector('.save-message', page=EDITOR_PAGE)
 
-    inp = notebook_frontend.editor_page.wait_for_selector('//input[@data-testid="save-as"]')
+    inp = notebook_frontend.wait_for_selector('//input[@data-testid="save-as"]', page=EDITOR_PAGE)
     inp.type('new_notebook.ipynb')
     notebook_frontend.try_click_selector('//html//body//div[8]//div//div//div[3]//button[2]', page=EDITOR_PAGE)
 
     check_for_rename(notebook_frontend, '#notebook_name', page=EDITOR_PAGE, new_name="new_notebook.ipynb")
 
-    # notebook_frontend.editor_page.query_selector('#notebook_name')
-    # notebook_frontend.editor_page.query_selector('#notebook_name')
-    # notebook_frontend.editor_page.query_selector('#notebook_name')
-    # notebook_frontend.editor_page.query_selector('#notebook_name')
-
     # Test that the name changed
     assert get_notebook_name(notebook_frontend) == "new_notebook.ipynb"
 
-    # Test that address bar was updated (TODO: get the base url)
-    print(f"editor_page dir: {dir(notebook_frontend.editor_page)}")
-    assert "new_notebook.ipynb" in notebook_frontend.editor_page.url
+    # Test that address bar was updated
+    assert "new_notebook.ipynb" in notebook_frontend.get_page_url(page=EDITOR_PAGE)
