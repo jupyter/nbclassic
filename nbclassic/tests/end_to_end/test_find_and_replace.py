e264193332039a3e14c7eaa4966093fabe4d58f2
Eric Gentry <eric.snek.bytes@gmail.com>
2022-10-19 21:11:43 -0400

    Refactor find/replace to use .value in eval call.

--- nbclassic/tests/end_to_end/test_find_and_replace.py
+++ nbclassic/tests/end_to_end/test_find_and_replace.py
@@ -11,10 +11,14 @@ def test_find_and_replace(notebook_frontend):
     find_str = "ello"
     replace_str = "foo"
 
+    notebook_frontend.wait_for_condition(lambda: notebook_frontend.get_cells_contents() == INITIAL_CELLS, timeout=120, period=5)
+
     # replace the strings
     notebook_frontend.find_and_replace(index=0, find_txt=find_str, replace_txt=replace_str)
 
     # check content of the cells
-    assert notebook_frontend.get_cells_contents() == [
-        s.replace(find_str, replace_str) for s in INITIAL_CELLS
-    ]
+    notebook_frontend.wait_for_condition(
+        lambda: notebook_frontend.get_cells_contents() == [
+            s.replace(find_str, replace_str) for s in INITIAL_CELLS
+        ]
+    )
