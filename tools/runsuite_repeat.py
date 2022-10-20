d677d6658eef6cc6f2b75f1f6e129a665a20b070
Eric Gentry <eric.snek.bytes@gmail.com>
2022-10-20 14:03:19 -0400

    More workflow edits.

--- tools/runsuite_repeat.py
+++ tools/runsuite_repeat.py
@@ -9,7 +9,11 @@ import time
 def run():
     for stepnum in range(10):
         print(f'[RUNSUITE_REPEAT] {os.getcwd()} :: {[os.path.exists("nbclassic"), os.path.exists("nbclassic/tests"), os.path.exists("nbclassic/tests/end_to_end"), os.path.exists("tools/runsuite_repeat.py")]}')
-        proc = subprocess.run('pytest -sv nbclassic/tests/end_to_end')
+        try:
+            proc = subprocess.run('pytest -sv tests/end_to_end')
+        except Exception:
+            print(f'\n[RUNSUITE_REPEAT] Run {stepnum} -> Exception')
+            continue
         print(f'\n[RUNSUITE_REPEAT] Run {stepnum} -> {"Success" if proc.returncode == 0 else proc.returncode}\n')
 
 
