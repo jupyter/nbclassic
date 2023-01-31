77385a0627a5a6afeffeffb6b7720355114b3296
Eric Gentry <ericsnekbytes@gmail.com>
2023-01-31 08:15:25 -0500

    Comments/formatting, code cleanup.

--- nbclassic/tests/end_to_end/test_save_readonly_as.py
+++ nbclassic/tests/end_to_end/test_save_readonly_as.py
@@ -30,21 +30,16 @@ def test_save_readonly_as(notebook_frontend):
 
     # Set a name for comparison later
     print('[Test] Set notebook name')
-    print('/IMG/Before set nb name/' + notebook_frontend._editor_page.screenshot().hex() + '/IMG/')
     set_notebook_name(notebook_frontend, name="nb1.ipynb")
-    print('/IMG/After set nb name/' + notebook_frontend._editor_page.screenshot().hex() + '/IMG/')
     assert get_notebook_name(notebook_frontend) == "nb1.ipynb"
 
     # Wait for Save As modal, save
     print('[Test] Save')
-    print('/IMG/Before save/' + notebook_frontend._editor_page.screenshot().hex() + '/IMG/')
     save_as(notebook_frontend)
-    print('/IMG/After save/' + notebook_frontend._editor_page.screenshot().hex() + '/IMG/')
 
     # # Wait for modal to pop up
     print('[Test] Waiting for modal popup')
     notebook_frontend.wait_for_selector(".modal-footer", page=EDITOR_PAGE)
-    print('/IMG/After wait modal popup/' + notebook_frontend._editor_page.screenshot().hex() + '/IMG/')
     dialog_element = notebook_frontend.locate(".modal-footer", page=EDITOR_PAGE)
     dialog_element.focus()
 
@@ -103,49 +98,9 @@ def test_save_readonly_as(notebook_frontend):
         print('[Test] Notebook name was changed!')
         return True
 
+    # Retry until timeout, this process is *very* flaky
     notebook_frontend.wait_for_condition(attempt_form_fill_and_save, timeout=900, period=1)
 
-    # print('/IMG/Before set field value/' + notebook_frontend._editor_page.screenshot().hex() + '/IMG/')
-    # # '(elem) => {{ elem.value = "new_notebook.ipynb"; return elem.value; }}'
-    # print(f"""[Test] VALUE :: {name_input_element.evaluate('(elem) => {{ return elem.value; }}')}""")
-    # print(f"""[Test] INPUT_COUNT :: {name_input_element._element.count()}""")
-    # print('/IMG/After set field value/' + notebook_frontend._editor_page.screenshot().hex() + '/IMG/')
-    #
-    # # Show the input field value
-    # print('[Test] Name input field contents:')
-    # print('[Test] ' + name_input_element.evaluate(f'(elem) => {{ return elem.value; }}'))
-
-    # # Try to ensure the save button is hidden (the prompt went away)
-    # print('[Test] Check save button visibility to ensure prompt is gone')
-    # if save_element.is_visible():
-    #     print('[Test] Save button visible, waiting for hidden...')
-    #     try:
-    #         save_element.expect_not_to_be_visible(timeout=30)
-    #     except Exception as err:
-    #         traceback.print_exc()
-    #         print('[Test] Failure waiting for save button hidden, see error above')
-    #
-    #     if save_element.is_visible():
-    #         try:
-    #             print('/IMG/Save btn waiting/' + notebook_frontend._editor_page.screenshot().hex() + '/IMG/')
-    #             print('[Test] Save button still visible! Likely error...')
-    #             save_message_element = notebook_frontend.locate('.modal-body .save-message', page=EDITOR_PAGE)
-    #             print('[Test] Contents of the save-message element:')
-    #             print('[Test] ' + save_message_element.get_inner_text())
-    #         except Exception as err:
-    #             traceback.print_exc()
-    #             print('\n[Test] Error retrieving save message (see above), continuing...')
-    # else:
-    #     print('[Test] Save button is hidden, continuing')
-
-    # print('[Test] Test notebook name change')
-    # print('/IMG/Before namechange/' + notebook_frontend._editor_page.screenshot().hex() + '/IMG/')
-    # # locator_element.expect_not_to_be_visible()
-    # notebook_frontend.wait_for_condition(
-    #     lambda: get_notebook_name(notebook_frontend) == "new_notebook.ipynb", timeout=120, period=5
-    # )
-    # print('/IMG/After namechange/' + notebook_frontend._editor_page.screenshot().hex() + '/IMG/')
-
-    print('[Test] Test address bar')
     # Test that address bar was updated
+    print('[Test] Test address bar')
     assert "new_notebook.ipynb" in notebook_frontend.get_page_url(page=EDITOR_PAGE)
