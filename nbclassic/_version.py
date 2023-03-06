d2b5fe7170fa6b8442e2fde9ee57516b970a33dc
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2023-03-06 07:04:21 +0000

    Bump to 0.6.0.dev0

--- nbclassic/_version.py
+++ nbclassic/_version.py
@@ -5,7 +5,7 @@ store the current version info of nbclassic.
 import re
 
 # Version string must appear intact for tbump versioning
-__version__ = '0.5.3'
+__version__ = '0.6.0.dev0'
 
 # Build up version_info tuple for backwards compatibility
 pattern = r'(?P<major>\d+).(?P<minor>\d+).(?P<patch>\d+)(?P<rest>.*)'
