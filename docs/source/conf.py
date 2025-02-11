477192d980b7b638e22c8d7f4aba20f3ae212127
Dan Yeaw <dan@yeaw.me>
2025-02-11 16:30:15 -0500

    Docs: cleanup warnings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,6 @@
 
 import sys
 import os
-import shlex
 import shutil
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -117,7 +116,7 @@ release = version_ns['__version__']
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,6 +1,6 @@
-==================
+=================
 Jupyter NbClassic
-==================
+=================
 
 :ref:`Getting Started <NbClassicUsage>`
 
