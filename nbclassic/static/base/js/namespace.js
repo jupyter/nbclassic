6c99f0dceab24dad90f00d24913cc05c99ea6b18
RRosio <RRosio@users.noreply.github.com>
2025-04-24 22:02:28 +0000

    Publish 1.3.0
    SHA256 hashes:
    
    nbclassic-1.3.0-py3-none-any.whl: 007e816187ed0ad5f165077ccfbcf60e271efe5949281b682e228436ff80afeb
    
    nbclassic-1.3.0.tar.gz: 1ae3b30a4d051332559b6d0eaba34c20ada51a73c95a7f35954c126e6a5c80fd

--- nbclassic/static/base/js/namespace.js
+++ nbclassic/static/base/js/namespace.js
@@ -73,7 +73,7 @@ define(function(){
     // tree
     jglobal('SessionList','tree/js/sessionlist');
 
-    Jupyter.version = "1.3.0.dev1";
+    Jupyter.version = "1.3.0";
     Jupyter._target = '_blank';
 
     return Jupyter;
