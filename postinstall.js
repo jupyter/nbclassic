122302e4a292351408ffd2a985ab5cbc43a51e37
RRosio <rosio_reyes@yahoo.com>
2025-07-30 15:04:58 -0700

    partial migration of JS dependencies to npm

--- postinstall.js
+++ postinstall.js
@@ -24,3 +24,31 @@ ensureSymlink(
   "node_modules/font-awesome",
   "nbclassic/static/components/font-awesome"
 );
+ensureSymlink(
+  "node_modules/backbone",
+  "nbclassic/static/components/backbone"
+);
+ensureSymlink(
+  "node_modules/bootstrap",
+  "nbclassic/static/components/bootstrap"
+);
+ensureSymlink(
+  "node_modules/bootstrap-tour",
+  "nbclassic/static/components/bootstrap-tour"
+);
+ensureSymlink(
+  "node_modules/jed",
+  "nbclassic/static/components/jed"
+);
+ensureSymlink(
+  "node_modules/moment",
+  "nbclassic/static/components/moment"
+);
+ensureSymlink(
+  "node_modules/text-encoding",
+  "nbclassic/static/components/text-encoding"
+);
+ensureSymlink(
+  "node_modules/underscore",
+  "nbclassic/static/components/underscore"
+);
